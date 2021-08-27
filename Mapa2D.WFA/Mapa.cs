c3d23afd93e9414fbd87448e42ec7dce293f2d39
Chronus Tecnologia <chronustecnologiasoftware@gmail.com>
2021-08-27 16:53:41 -0300

    ajustes

--- Mapa2D.WFA/Mapa.cs
+++ Mapa2D.WFA/Mapa.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MapaGabriel
+namespace Mapa2D.WFA
 {
     public class Posicao
     {
